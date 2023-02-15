0ea32955e16fc99c874c76cb01a795c786717e35
steins <s2453812462@gmail.com>
2023-02-15 12:22:57 +0800

    change

--- npc/obj_dir/Vdut___024unit.cpp
+++
@@ -1,8 +0,0 @@
-// Verilated -*- C++ -*-
-// DESCRIPTION: Verilator output: Design implementation internals
-// See Vdut.h for the primary calling header
-
-#include "Vdut___024unit.h"
-#include "Vdut__Syms.h"
-
-//==========
